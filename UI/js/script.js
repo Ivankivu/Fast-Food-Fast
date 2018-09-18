018c971d6cd10b1a8316bd358a4f9fb5b1336261
Ivankivu <ivankivu@gmail.com>
2018-09-18 11:36:51 +0300

    final update for UI
    [(delivers) #160446336]

--- UI/js/script.js
+++ UI/js/script.js
@@ -74,24 +74,18 @@ if (tbl != null) {
 //  checkout modal
   var modal = document.getElementById('myModal');
 
-  // Get the button that opens the modal
   var btns = document.querySelectorAll(".addBtn");
 
-  // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks the button, open the modal 
   [].forEach.call(btns, function (el) {
       el.onclick = function () {
           modal.style.display = "block";
       }
   })
-  // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
       modal.style.display = "none";
   }
-
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
       if (event.target == modal) {
           modal.style.display = "none";
