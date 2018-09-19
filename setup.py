1bde0fc6b330fce82a0bd89d7bc88a3aca53e9ac
Ivankivu <ivankivu@gmail.com>
2018-09-19 16:40:01 +0300

    finally uploaded

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
-# setup.py
-from setuptools.command.test import test as TestCommand
+from setuptools import setup, find_packages
 
-
-class PyTest(TestCommand):
-
-    """Setup the py.test test runner."""
-
-    def finalize_options(self):
-        """Set options for the command line."""
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        """Execute the test runner command."""
-        # Import here, because outside the required eggs aren't loaded yet
-        import pytest
-        sys.exit(pytest.main(self.test_args))
+setup(
+    name='Fast-Food-Fast',
+    version='1.0',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask'],
+)
