f0d7e4ddc7798de138dae6fe38b351c9c1008b7b
Ivankivu <ivankivu@gmail.com>
2018-09-24 09:58:14 +0300

    Add place a new order for food-endpoint
    [delivers #160608280]

--- main/tests/test_orders.py
+++ main/tests/test_orders.py
@@ -5,6 +5,7 @@ from main.config import app_config
 from main.views.orders import app
 from flask import json
 
+
 class OnlineRestuarantTest(unittest.TestCase):
 
     def create_app(self):
@@ -13,7 +14,8 @@ class OnlineRestuarantTest(unittest.TestCase):
 
     def setUp(self):
         self.client = app.test_client(self)
-        self.order ={ "Food": "Rice",
+        self.order = {
+            "Food": "Rice",
             "amount": 1500,
             "available": "pending",
             "order_id": 9
@@ -22,36 +24,36 @@ class OnlineRestuarantTest(unittest.TestCase):
     def test_get_all_orders(self):
         orders = self.client.get(
             '/api/v1/orders',
-            content_type = 'application/json',
-            data = json.dumps(self.order)
+            content_type='application/json',
+            data=json.dumps(self.order)
         )
-       # data = json.loads(orders.data.decode())
-        self.assertEqual(orders.available_code,200)
-        self.assertIn('"order_id":9',str(orders.data))
+        # data = json.loads(orders.data.decode())
+        self.assertEqual(orders.status_code, 200)
+        self.assertIn('"order_id":9', str(orders.data))
 
     def test_ii_post_order(self):
         order_list = []
         order1 = self.client.post(
             '/api/v1/orders',
-            content_type = 'application/json',
-            data = json.dumps(self.order)
+            content_type='application/json',
+            data=json.dumps(self.order)
         )
-        order ={ "Food": "Rice",
+        order = {
+            "Food": "Rice",
             "amount": 1500,
             "available": "pending",
             "order_id": 9
         }
 
         order_list.append(order)
-        self.assertEqual(order1.available_code,201)
-        self.assertIn('"order_id":9',str(order1.data))
+        self.assertEqual(201, order1.status_code)
+        self.assertIn('"order_id":9', str(order1.data))
 
     def test_welcome(self):
         self.assertTrue(True)
 
     def test_get_orders(self):
         raise ValueError("orders not found")
-       
 
     def test_not_found(self):
         pass
--- main/views/orders.py
+++ main/views/orders.py
@@ -1,58 +1,62 @@
-from flask import Flask, request, jsonify, json, make_response, Response, abort, make_response
+from flask import Flask, request, jsonify, json, make_response, Response
 from main.controllers.new_order import Order
 
 
 app = Flask(__name__)
 
-orderlist = Order(12,'oio',455,True).list_order()
+orderlist = Order(12, 'oio', 455, True).list_order()
+
 
 class OnlineRestuarant():
-    
 
     @app.route("/", methods=["GET"])
     def Home():
         return '''
-        <div style="text-align:center;"><h2 style="font-size:70px;"> <span style="color:orange;">Fast Food Fast</span> Delivery app -API</h2>
-        </div><div style="text-align:center;"><a style="margin-top:400px;text-decoration:none;border:1px solid orange;border-radius:15px;padding:50px;" href="https://fastfood-fast-api-heroku.herokuapp.com/api/v1/">next page</a>
+        <div style="text-align:center;"><h2 style="font-size:70px;">
+        <span style="color:orange;">Fast Food Fast</span>
+         Delivery app -API</h2>
+        </div><div style="text-align:center;">
+        <a style="margin-top:400px;text-decoration:none;
+        border:1px solid orange;border-radius:15px;padding:50px;"
+         href="https://fastfood-fast-api-heroku.herokuapp.com/api/v1/">
+         next page</a>
         </div>
         '''
-        
+
     @app.route("/api/v1/", methods=["GET"])
     def welcome():
         return make_response(jsonify({"message": "You are most welcome!"}))
 
     @app.route("/api/v1/orders", methods=["GET"])
     def get_orders():
-        
+
         if orderlist == []:
             return "orders not found", 404
-
-        
-        return jsonify({'orders':orderlist}), 200
+        return jsonify({'orders': orderlist}), 200
 
     @app.route("/api/v1/orders/<int:order_id>", methods=["GET"])
     def get_order(order_id):
-        
+
         if orderlist == []:
             return "orders not found", 404
 
-        ods =[order for order in orderlist if order['order_id'] == order_id]
+        ods = [order for order in orderlist if order['order_id'] == order_id]
         if not ods:
             return jsonify({"error": "no match found"})
 
-        return jsonify({'order':ods[0]}), 200
+        return jsonify({'order': ods[0]}), 200
 
     @app.route("/api/v1/orders", methods=["POST"])
-    def add_order(order_id):
+    def add_order():
         data = request.get_json()
         for data in orderlist:
             if data['order_id'] == order_id:
-                return jsonify({'error':"already exists"})
+                return jsonify({'error': "already exists"})
         order = {
-                'order_id':data['order_id'],
-                'Food':data['Food'],
-                'amount':data['amount'],
-                'available':data['available']
+                'order_id': data['order_id'],
+                'Food': data['Food'],
+                'amount': data['amount'],
+                'available': data['available']
                 }
         orderlist.append(order)
         return jsonify({'orderlist': orderlist}), 201
@@ -61,24 +65,25 @@ class OnlineRestuarant():
     def edit_order(order_id):
         data = request.get_json()
         order1 = {
-                'order_id':data['order_id'],
-                'Food':data['Food'],
-                'amount':data['amount'],
-                'available':data['available']
+                'order_id': data['order_id'],
+                'Food': data['Food'],
+                'amount': data['amount'],
+                'available': data['available']
                 }
-        ods =[order for order in orderlist if order['order_id'] == order_id]
+        ods = [order for order in orderlist if order['order_id'] == order_id]
         ods[0]['available'] = data['available']
         ods[0] = order1
         return jsonify({'orderlist': orderlist}), 200
 
     @app.route("/api/v1/orders/<int:order_id>", methods=["DELETE"])
     def remove_order(order_id):
-       od =[order for order in orderlist if order['order_id'] == order_id]
-       if not od:
-           return jsonify({'error':'order does not exist'})
 
-       orderlist.remove(od[0])
-       return jsonify({'orderlist': orderlist}),200
+        od = [order for order in orderlist if order['order_id'] == order_id]
+        if not od:
+            return jsonify({'error': 'order does not exist'})
+
+        orderlist.remove(od[0])
+        return jsonify({'orderlist': orderlist}), 200
 
     @app.errorhandler(404)
     def not_found(e):
