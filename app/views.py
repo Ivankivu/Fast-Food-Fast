515ab70b4583014ac455a4bca17e5d3ba9654c29
Ivankivu <ivankivu@gmail.com>
2018-09-27 16:31:51 +0300

    modified model.py
    [finishes #160608314]

--- app/views.py
+++ app/views.py
@@ -23,54 +23,35 @@ class OnlineRestuarant():
     @app.route("/api/v1/", methods=["GET"])
     def welcome():
 
-        '''
-            This is the first page
-        '''
-
         return make_response(jsonify({"message": "You are most welcome!"}))
 
     @app.route("/api/v1/orders", methods=["GET"])
     def get_orders():
 
-        '''
-         Retrive/get existing data(orders) from a list
-        '''
         response = Order.get_all_orders()
         return response
 
     @app.route("/api/v1/orders/<int:order_id>", methods=["GET"])
     def get_order(order_id):
 
-        '''
-            Retrive/get a single data(order) from a list
-        '''
         response = Order.get_order_by_id(order_id)
         return response
 
     @app.route("/api/v1/orders", methods=["POST"])
     def add_order():
 
-        '''
-            send/post data(an order) to a list
-        '''
         response = Order.create_order()
         return response
 
     @app.route("/api/v1/orders/<int:order_id>", methods=["PUT"])
     def edit_order(order_id):
 
-        '''
-            change/edit existing data(an order) in a list
-        '''
         response = Order.change_order_status(order_id)
         return response
 
     @app.route("/api/v1/orders/<int:order_id>", methods=["DELETE"])
     def remove_order(order_id):
 
-        '''
-            Remove/delete data(an order) from a list
-        '''
         response = Order.delete_order(order_id)
         return response
 
