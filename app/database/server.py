e220b927f14df433877a0c914a7c8c992b18ed8e
Ivan Kivumbi <ivankivu@gmail.com>
2018-10-05 15:12:18 +0300

    Update server.py

--- app/database/server.py
+++ app/database/server.py
@@ -2,30 +2,15 @@ import psycopg2
 import os
 from pprint import pprint
 from app.config import app_config
-# from .tables import MyTables
 
 
 class DBConnection():
 
     def __enter__(self):
         try:
-            #  self.conn = psycopg2.connect('dbname = 'fastfood' user = 'postgres' host = 'localhost' password = 'andela' port = '5432')
-            # #     #dbname = 'fastfood' user = 'postgres' host = 'localhost' password = 'andela' port = '5432
             self.conn = psycopg2.connect('dbname = 'd3oceli53i6eg7' user = 'hscqqcrrhcgcpn' host = 'ec2-54-235-90-0.compute-1.amazonaws.com' password = '30b46f80b9e83ac37a9fca1545fdda836058d1e73f52a7723fb51d77af4dc3a1' port = '5432')
             self.cursor = self.conn.cursor()
             return self.cursor
-            print("Database created successfully")
-        except (Exception, psycopg2.DatabaseError) as error:
-            pprint(error)
-
-    def __exit__(self, exception_type, exception_val, exception_traceback):
-        self.conn.commit()
-        self.cursor.close()
-        self.conn.close()
-
-            self.cursor = self.conn.cursor()
-            return self.cursor
-            print("Database created successfully")
         except (Exception, psycopg2.DatabaseError) as error:
             pprint(error)
 
